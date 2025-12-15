6236852925acb46d5f9ab8685605ed54758c947e
KMoran00 <kirshamoranest@gmail.com>
2025-12-15 12:23:25 +0100

    Sesión 2 - Ejercicio 1 acabado

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,10 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name ('inicio');
 
+//Sesión 1: creación ruta
 Route::get('/posts', function(){
     return "Listado de posts ";
-});
+})->name('posts_listado');
+
+//Sesión 2: EJERCICIO 1
+Route::get('/posts/{id}', function ($id){
+    return "Ficha del post ".$id;
+})-> where('id', "[0-9]")
+->name('posts_ficha');
+
 
 ?>
