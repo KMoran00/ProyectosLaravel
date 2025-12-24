162d1e30667de74d31f909c473a6912a61b644a7
KMoran00 <kirshamoranest@gmail.com>
2025-12-24 17:52:29 +0000

    migración creada

---
+++ blog/database/migrations/crear_tabla_posts.php
@@ -0,0 +1,29 @@
+<?php
+//Migración creada en la Sesión 4 Ej1
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->integer('id')->primary()->auto_increment();
+            $table->string('titulo');
+            $table->text('contenido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
